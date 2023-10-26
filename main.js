3068e9e5e4155f35135d8dd450df0137d358e561
Du <110917469+EduardoDalPozzo@users.noreply.github.com>
2023-10-26 09:08:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+
+const tiles = Array.from(document.querySelectorAll('.bloco'));
+const playerDisplay = document.querySelector('.display-player');
+const resetButton = document.querySelector('#reset');
+const announcer = document.querySelector('.announcer');
